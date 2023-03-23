3923ac52aa8b3077e273b0ee6168e88a7fd3b73e
liujialun <liujialun@lf-network.com>
2023-03-23 02:29:14 +0000

    3.23-1完善昨日代码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 use Overtrue\EasySms\EasySms;
+use Illuminate\Support\Facades\Cache;
 use App\Http\Requests\Api\VerificationCodeRequest;
 use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
 
@@ -13,12 +13,15 @@ class VerificationCodesController extends Controller
     public function store(VerificationCodeRequest $request,EasySms $easySms)
     {
         $phone = $request->phone;
+
+        //如果非生产环境，code=1234
         if (!app()->environment('production')) {
             $code = '1234';
         } else{
         //生成4位随机数，左侧补0
         $code = Str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
+        //使用send方法，register短信模板，把code通过阿里云发送到用户手机
         try {
             $request = $easySms->send($phone, [
                 'template' => config('easysms.gateways.aliyun.templates.register'),
@@ -26,16 +29,21 @@ class VerificationCodesController extends Controller
                     'code' => $code
                 ],
             ]);
-        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+        } catch (NoGatewayAvailableException $exception) {
             $message = $exception->getException('aliyun')->getMessage();
-            abort(500, $message ?: '短信发送一场');
+            abort(500, $message ?: '短信发送异常');
         }
     }
+        //生成个15位随机数key
         $key = Str::random(15);
+
+        //配置缓存key、缓存过期时间：5分钟
         $cacheKey = 'verificationCode_'.$key;
         $expiredAt = now()->addMinutes(5);
-        //缓存验证码5分钟过期
-        \Cache::put($cacheKey, ['phone' => $phone, 'code' => $code], $expiredAt);
+        //Cache::put（$key, $value, $expiration）键值、过期时间
+        Cache::put($cacheKey, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        //返回201，15位随机数key和过期时间
         return response()->json([
             'key' => $key,
             'expired_at' => $expiredAt->toDateTimeString(),
